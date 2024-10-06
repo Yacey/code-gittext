4ceeac58fd31cae3c1dae08c6ea30eb3b75e5f30
JasonWarner <9655826+jasonwarner@user.noreply.gitee.com>
2024-10-06 22:06:55 +0800

    增加了一行6666

--- src/main/java/org/example/gittext.java
+++ src/main/java/org/example/gittext.java
@@ -5,6 +5,7 @@ public class gittext {
     public static void main(String[] args) {
         System.out.println("Hello worlld");
         System.out.println("Hello worl1236546");
+        System.out.println("6666");
     }
 
 }
