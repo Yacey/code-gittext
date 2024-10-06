0a769cc17e97e628db3d6cfaf2e006d77517f0ba
JasonWarner <9655826+jasonwarner@user.noreply.gitee.com>
2024-10-06 15:20:52 +0800

    删除了一点点东西

--- src/main/java/org/example/gittext.java
+++ src/main/java/org/example/gittext.java
@@ -3,7 +3,7 @@ package org.example;
 public class gittext {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello worl");
     }
 
 }
